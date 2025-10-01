5a24be4ac6d7c0c004e88255f2593207b3fbd342
Eduardo <ebialas@Eduardos-MacBook-Air.local>
2025-10-01 20:48:01 -0300

    Desenvolvido a tela de dashboard da industria responsiva

--- components/CardData/CardData.tsx
+++ components/CardData/CardData.tsx
@@ -3,34 +3,23 @@ import styles from './CardData.module.css';
 
 type CardDataProps = {
   icon: ReactNode;
+  label: string;
+  value: string | number;
 };
 
-
-export default function CardData({ icon }: CardDataProps) {
+export default function CardData({ icon, label, value }: CardDataProps) {
   return (
     <div className={styles.card_data}>
-        <div className={styles.card_content_left}>
-            <div className={styles.card_icon}>
-                {icon}
-            </div>
+      <div className={styles.card_content_left}>
+        <div className={styles.card_icon}>{icon}</div>
+      </div>
+      <div className={styles.card_content_right}>
+        <div className={styles.card_label}>{label}</div>
+        <div className={styles.card_info}>
+          <div className={styles.card_value}>{value}</div>
+          {/* badge removido para simplificar, adicione se quiser */}
         </div>
-        <div className={styles.card_content_right}>
-            <div className={styles.card_label}>
-                Total de vendas
-            </div>
-
-            <div className={styles.card_info}>
-                <div className={styles.card_value}> 
-                    24
-                </div>
-                <div className={styles.card_badge}>
-                    <span>
-                        +5%
-                    </span>
-                </div>
-            </div>
-
-        </div>
+      </div>
     </div>
   );
 }
